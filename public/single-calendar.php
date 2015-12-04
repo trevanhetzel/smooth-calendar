bae2d25d6838de05092aff44c16bdcaf79552134
Trevan Hetzel <trevan@hetzelcreative.com>
2015-12-03 19:11:52 -0600

    Redirect to home page if single event pages are disabled

--- public/single-calendar.php
+++ public/single-calendar.php
@@ -1,49 +1,55 @@
 <?php
+$single = get_option('calendar_setting_single');
 
-if ( $overridden_template = locate_template( 'single-calendar.php' ) ) {
-   // locate_template() returns path to file
-   // if either the child theme or the parent theme have overridden the template
-   load_template( $overridden_template );
- } else {
+if ($single) {
+  if ( $overridden_template = locate_template( 'single-calendar.php' ) ) {
+     // locate_template() returns path to file
+     // if either the child theme or the parent theme have overridden the template
+     load_template( $overridden_template );
+   } else {
 
-  get_header();
+    get_header();
 
-  while ( have_posts() ) : the_post();
+    while ( have_posts() ) : the_post();
 
-  the_title( '<h1 class="entry-title">', '</h1>' );
+    the_title( '<h1 class="entry-title">', '</h1>' );
 
-  $date = get_post_meta( $post->ID, 'meta_calendar_date', true );
-  $startTime = get_post_meta( $post->ID, 'meta_calendar_start', true );
-  $endTime = get_post_meta( $post->ID, 'meta_calendar_end', true );
-  $location = get_post_meta( $post->ID, 'meta_calendar_location', true );
-  $description = get_post_meta( $post->ID, 'meta_calendar_description', true );
+    $date = get_post_meta( $post->ID, 'meta_calendar_date', true );
+    $startTime = get_post_meta( $post->ID, 'meta_calendar_start', true );
+    $endTime = get_post_meta( $post->ID, 'meta_calendar_end', true );
+    $location = get_post_meta( $post->ID, 'meta_calendar_location', true );
+    $description = get_post_meta( $post->ID, 'meta_calendar_description', true );
 
-  if ($date) {
-    echo '<p><strong>Date:</strong> ' . $date . '</p>';
-  }
-
-  if ($startTime) {
-    echo '<p><strong>Time:</strong> ' . $startTime;
-
-    if ($endTime) {
-      echo ' - ' . $endTime . '</p>';
-    } else {
-      echo '</p>';
+    if ($date) {
+      echo '<p><strong>Date:</strong> ' . $date . '</p>';
     }
+
+    if ($startTime) {
+      echo '<p><strong>Time:</strong> ' . $startTime;
+
+      if ($endTime) {
+        echo ' - ' . $endTime . '</p>';
+      } else {
+        echo '</p>';
+      }
+    }
+
+    if ($location) {
+      echo '<p><strong>Location:</strong> ' . $location . '</p>';
+    }
+
+    if ($description) {
+      echo $description;
+    }
+
+    endwhile;
+
+    get_footer();
+
   }
-
-  if ($location) {
-    echo '<p><strong>Location:</strong> ' . $location . '</p>';
-  }
-
-  if ($description) {
-    echo $description;
-  }
-
-  endwhile;
-
-  get_footer();
-
+} else {
+  wp_redirect( home_url() );
+  exit;
 }
 
 ?>
